aefc453308b6f7b9b16142c6cf39bedfa9688eca
Wilgnert de Alcântara <wilgnert@hotmail.com>
2023-11-15 17:53:24 -0300

    Commit 1 - Extração de Função

--- index.js
+++ index.js
@@ -10,25 +10,7 @@ function gerarFaturaStr (fatura, pecas) {
   
     for (let apre of fatura.apresentacoes) {
       const peca = pecas[apre.id];
-      let total = 0;
-  
-      switch (peca.tipo) {
-      case "tragedia":
-        total = 40000;
-        if (apre.audiencia > 30) {
-          total += 1000 * (apre.audiencia - 30);
-        }
-        break;
-      case "comedia":
-        total = 30000;
-        if (apre.audiencia > 20) {
-           total += 10000 + 500 * (apre.audiencia - 20);
-        }
-        total += 300 * apre.audiencia;
-        break;
-      default:
-          throw new Error(`Peça desconhecia: ${peca.tipo}`);
-      }
+      let total = calcularTotalApresentacao(apre, peca);
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
@@ -42,6 +24,29 @@ function gerarFaturaStr (fatura, pecas) {
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
     faturaStr += `Créditos acumulados: ${creditos} \n`;
     return faturaStr;
+
+  function calcularTotalApresentacao(apre, peca) {
+    let total = 0;
+
+    switch (peca.tipo) {
+      case "tragedia":
+        total = 40000;
+        if (apre.audiencia > 30) {
+          total += 1000 * (apre.audiencia - 30);
+        }
+        break;
+      case "comedia":
+        total = 30000;
+        if (apre.audiencia > 20) {
+          total += 10000 + 500 * (apre.audiencia - 20);
+        }
+        total += 300 * apre.audiencia;
+        break;
+      default:
+        throw new Error(`Peça desconhecia: ${peca.tipo}`);
+    }
+    return total;
+  }
   }
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
